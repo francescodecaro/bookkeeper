49e472b61610d4e53a28207699024e7bf9ddb72d
Francesco De Caro <francescodecaro96@gmail.com>
2022-01-06 12:21:33 +0100

    wip first test class

---
+++ bookkeeper-server/src/test/java/it/uniroma2/dicii/isw2/Utils.java
@@ -0,0 +1,25 @@
+package it.uniroma2.dicii.isw2;
+
+public class Utils {
+
+    public static byte[] toPrimitives(Byte[] oBytes)
+    {
+        byte[] bytes = new byte[oBytes.length];
+
+        for(int i = 0; i < oBytes.length; i++) {
+            bytes[i] = oBytes[i];
+        }
+
+        return bytes;
+    }
+
+    // byte[] to Byte[]
+    public static Byte[] toObjects(byte[] bytesPrim) {
+        Byte[] bytes = new Byte[bytesPrim.length];
+
+        int i = 0;
+        for (byte b : bytesPrim) bytes[i++] = b; // Autoboxing
+
+        return bytes;
+    }
+}
