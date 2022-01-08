61bfef0c4f6bda562c34eb711a25dd25803bfbf7
Francesco De Caro <francescodecaro96@gmail.com>
2022-01-08 18:44:39 +0100

    instruction coverage 75%, branch coverage 60%

--- bookkeeper-server/src/test/java/it/uniroma2/dicii/isw2/LedgerCreateOpTest.java
+++ bookkeeper-server/src/test/java/it/uniroma2/dicii/isw2/LedgerCreateOpTest.java
@@ -2,16 +2,17 @@ package it.uniroma2.dicii.isw2;
 
 import org.apache.bookkeeper.client.BKException;
 import org.apache.bookkeeper.client.api.DigestType;
+import org.apache.bookkeeper.client.api.WriteAdvHandle;
+import org.apache.bookkeeper.client.api.WriteFlag;
 import org.apache.bookkeeper.client.api.WriteHandle;
+
 import org.apache.bookkeeper.test.BookKeeperClusterTestCase;
-import org.hamcrest.Matcher;
-import org.hamcrest.MatcherAssert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 
 import static org.hamcrest.Matchers.hasItem;
@@ -25,33 +26,57 @@ public class LedgerCreateOpTest extends BookKeeperClusterTestCase {
     private int ensSize;
     private int writeQuorumSize;
     private int ackQuorumSize;
-//    private Map<String, byte[]> customMetadata;
     private DigestType digestType;
     private String password;
+    private Map<String, byte[]> customMetadata;
+    private long ledgerId;
     private int exceptionCode;
+    private boolean opportunisticStriping;
+    private boolean systemTime;
 
     @Parameterized.Parameters
     public static Collection params() {
+        Map<String, byte[]> customMetadata = new HashMap<>();
+        customMetadata.put("key", "value".getBytes());
         return Arrays.asList(new Object[][]{
-                {0, 1, 2, null, null, BKException.Code.IncorrectParameterException },
-                {1, 1, 1, DigestType.CRC32, "password", BKException.Code.OK }
-//                { 3, 2, 1, Collections.emptyMap(), BKException.Code.OK },
-//                { 1, 1, 1, Collections.emptyMap(), BKException.Code.OK },
-//                { 1, 2, 2, Collections.emptyMap(), BKException.Code.IncorrectParameterException },
-//                { 2, 1, 2, Collections.emptyMap(), BKException.Code.IncorrectParameterException },
-//                { 4, 2, 1, Collections.emptyMap(), BKException.Code.NotEnoughBookiesException },
-//                { 1, 1, 1, null, BKException.Code.IncorrectParameterException }
+                {0, 1, 2, null, null, Collections.emptyMap(), 0, BKException.Code.IncorrectParameterException, false, true },
+                {1, 1, 1, DigestType.CRC32, "password", Collections.emptyMap(),  0, BKException.Code.OK, false, true },
+                {1, 1, 1, DigestType.CRC32, "", Collections.emptyMap(), 0, BKException.Code.OK, false, false },
+                { 3, 2, 1, DigestType.CRC32C, "password", Collections.emptyMap(), 0, BKException.Code.OK, true, false },
+                { 3, 2, 1, DigestType.MAC, "password", Collections.emptyMap(), 0, BKException.Code.OK, false, false },
+                { 3, 2, 1, DigestType.DUMMY, "password", customMetadata, 0, BKException.Code.OK, true, true },
+
+                { 3, 2, 1, DigestType.DUMMY, "password", customMetadata, -1L, BKException.Code.IncorrectParameterException, true, false },
+
+
+                { 4, 2, 1, DigestType.DUMMY, "password", customMetadata, 0, BKException.Code.NotEnoughBookiesException, false, true },
+
+
         });
     }
 
-    public LedgerCreateOpTest(int ensSize, int writeQuorumSize, int ackQuorumSize, DigestType digestType, String password, int exceptionCode) {
+    public LedgerCreateOpTest(int ensSize, int writeQuorumSize, int ackQuorumSize, DigestType digestType,
+                              String password, Map<String, byte[]> customMetadata,
+                              long ledgerId, int exceptionCode, boolean opportunisticStriping, boolean systemTime) {
         super(3);
         this.ensSize = ensSize;
         this.writeQuorumSize = writeQuorumSize;
         this.ackQuorumSize = ackQuorumSize;
         this.digestType = digestType;
         this.password = password;
+        this.customMetadata = customMetadata;
+        this.ledgerId = ledgerId;
         this.exceptionCode = exceptionCode;
+        this.opportunisticStriping = opportunisticStriping;
+        this.systemTime = systemTime;
+    }
+
+    @Before
+    @Override
+    public void setUp() throws Exception {
+        baseClientConf.setOpportunisticStriping(opportunisticStriping);
+        baseClientConf.setStoreSystemtimeAsLedgerCreationTime(systemTime);
+        setUp("/ledgers");
     }
 
     @Test
@@ -63,20 +88,58 @@ public class LedgerCreateOpTest extends BookKeeperClusterTestCase {
                     .withPassword(password.getBytes())
                     .withWriteQuorumSize(writeQuorumSize)
                     .withAckQuorumSize(ackQuorumSize)
+                    .withCustomMetadata(customMetadata)
+                    .withWriteFlags(WriteFlag.DEFERRED_SYNC)
                     .execute()          // execute the creation op
                     .get();
 
-//            assertEquals(customMetadata.keySet(), wh.getLedgerMetadata().getCustomMetadata().keySet());
-//            customMetadata.keySet().forEach(k -> {
-//                assertArrayEquals(customMetadata.get(k), wh.getLedgerMetadata().getCustomMetadata().get(k));
-//            });
+            assertEquals(customMetadata.keySet(), wh.getLedgerMetadata().getCustomMetadata().keySet());
+            customMetadata.keySet().forEach(k -> {
+                assertArrayEquals(customMetadata.get(k), wh.getLedgerMetadata().getCustomMetadata().get(k));
+            });
 
             assertEquals(wh.getLedgerMetadata().getState().name(), OPEN_STATE);
             assertFalse(wh.isClosed());
 
         } catch (NullPointerException e){
             assertThat(Arrays.asList(digestType, password), hasItem(nullValue()));
-//            assertNull(digestType);
+        } catch (InterruptedException e) {
+            fail("Test Timeout");
+        } catch (ExecutionException e) {
+            if (exceptionCode == BKException.Code.OK || !(e.getCause() instanceof BKException)){
+                fail();
+            }
+            BKException cause = (BKException) e.getCause();
+            assertEquals(exceptionCode, cause.getCode());
+        }
+    }
+
+    @Test
+    public void createAdvTest() {
+        try {
+            WriteAdvHandle wh = bkc.newCreateLedgerOp()
+                    .withEnsembleSize(ensSize)
+                    .withDigestType(digestType)
+                    .withPassword(password.getBytes())
+                    .withWriteQuorumSize(writeQuorumSize)
+                    .withAckQuorumSize(ackQuorumSize)
+                    .withCustomMetadata(customMetadata)
+                    .withWriteFlags(WriteFlag.DEFERRED_SYNC)
+                    .makeAdv()
+                    .withLedgerId(ledgerId)
+                    .execute()          // execute the creation op
+                    .get();
+
+            assertEquals(customMetadata.keySet(), wh.getLedgerMetadata().getCustomMetadata().keySet());
+            customMetadata.keySet().forEach(k -> {
+                assertArrayEquals(customMetadata.get(k), wh.getLedgerMetadata().getCustomMetadata().get(k));
+            });
+
+            assertEquals(wh.getLedgerMetadata().getState().name(), OPEN_STATE);
+            assertFalse(wh.isClosed());
+
+        } catch (NullPointerException e){
+            assertThat(Arrays.asList(digestType, password), hasItem(nullValue()));
         } catch (InterruptedException e) {
             fail("Test Timeout");
         } catch (ExecutionException e) {
